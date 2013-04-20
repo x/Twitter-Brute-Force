c66fd2e529018834677e8dbb3172f7be1fdac742
Devon Peticolas <devon.peticolas@gmail.com>
2013-04-20 01:13:52 -0400

    status now updates with sigusr1

--- brute.js
+++ brute.js
@@ -1,33 +1,51 @@
 var request = require('request')
   , fs = require('fs')
   , async = require('async')
-  , log = fs.createWriteStream('./error-log')
   , available = fs.createWriteStream('./available')
+  , fileNames = process.argv.splice(2)
+  , processed = 0
+  , log
+  , reservedWords = ['post', 'session', 'join', 'apps', 'auth', 'list']
 
+  /* queue of requests with a pool of 10 async */
   , q = async.queue(function(task, callback) {
       request('http://www.twitter.com/' + task.name, function (err, res) {
-        if(res.statusCode == 404) {
+        if(res && res.statusCode == 404) {
           available.write(task.name + '\n')
         }
         callback(err)
       })
     }, 10)
 
-process.argv.splice(2).forEach(function(fileName) {
-  
+/* catch sigusr1 for an update */
+process.on('SIGUSR1', function(){
+  console.log('only '+processed +' handles have been processed. Press Ctrl+D to exit')
+})
+
+/* read all dictionaries */
+fileNames.forEach(function(fileName) {
   fs.readFile('./' + fileName, 'utf8', function(err, data) {
-    if(err) {
-      return console.log(err);
-    }
+    if(err) { return console.log(err) }
+    
+    /* split file by lines */
     data.split('\n').forEach(function(line) {
-      if(line.length > 1) {
-        q.push({name: line}, function(err) {
-          if(err) {
-            log.write('\n\nsomething weird happened with '+line+'\n')
-            log.write(err)
-          }
-        })
+      if(line < 4) { 
+        console.log('ignoring '+line+' because 3 or less handles are all taken')
+        return
       }
+      if(reservedWords.indexOf(line) > 0) {
+        console.log('ignoring '+line+' because it\'t a reserved by twitter')
+      }
+
+      /* push each word */
+      q.push({name: line}, function(err) {
+        if(err) {
+          log = log || fs.createWriteStream('./error-log')
+          log.write('\n\nsomething weird happened with '+line+'\n')
+          log.write(err)
+        }
+        processed++
+      })
     })
   })
 })
